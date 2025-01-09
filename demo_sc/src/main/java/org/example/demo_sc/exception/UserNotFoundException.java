a883a31d6ccaf62f572782f77e241d5e9d49adb5
AnJ5oon <dkswogns010@a.ut.ac.kr>
2025-01-09 15:45:28 +0900

    feat:첨부파일기능 추가 수정

--- demo_sc/src/main/java/org/example/demo_sc/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.demo_sc.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
